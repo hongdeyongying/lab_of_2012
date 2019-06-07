742acfd1a512eccafeb24297c7d4c370e47cf4b9
gaoyong <gaoyong@wucar.com.cn>
2019-06-07 16:33:23 +0800

    path to third ,dep SOFT

--- SOFT/admin/ugw_start.sh
+++ SOFT/admin/ugw_start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-sudo  /usr/sbin/nginx -c /etc/nginx/nginx.conf 
+#sudo  /usr/sbin/nginx -c /etc/nginx/nginx.conf 
 
-service mysql start
+#service mysql start
 
-CONNSTR=127.0.0.1:8080
-INNER_CONNSTR=127.0.0.1:8081
+CONNSTR=${SOFT}UGW/bin/sock.unix.8080
+INNER_CONNSTR=${SOFT}UGW/bin/sock.unix.8081
 
-SOFT=/home/release/SOFT/
+#SOFT=/home/release/SOFT/
 
-echo "${SOFT}fcgi/bin/cgi-fcgi -start -connect ${CONNSTR} ${SOFT}UGW/bin/ugw  10"
-pwd_cur=`pwd`
-cd ${SOFT}/UGW/bin
-${SOFT}fcgi/bin/cgi-fcgi -start -connect ${CONNSTR} ${SOFT}UGW/bin/ugw  10
+echo "cgi-fcgi -start -connect ${CONNSTR} ${SOFT}UGW/bin/ugw  10"
+#pwd_cur=`pwd`
+#cd ${SOFT}/UGW/bin
+cgi-fcgi -start -connect ${CONNSTR} ${SOFT}UGW/bin/ugw  10
 
-${SOFT}fcgi/bin/cgi-fcgi -start -connect ${INNER_CONNSTR} ${SOFT}INNER_UGW/bin/inner_ugw  10
-cd $pwd_cur
+cgi-fcgi -start -connect ${INNER_CONNSTR} ${SOFT}INNER_UGW/bin/inner_ugw  10
+#cd $pwd_cur
